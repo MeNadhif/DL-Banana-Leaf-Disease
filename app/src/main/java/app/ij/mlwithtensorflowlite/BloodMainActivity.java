008610f24fa283bf7b47730c4941c21cee9d6f70
MeNadhif <nadhif1495@gmail.com>
2023-08-18 15:45:13 +0700

    initial commit

---
+++ app/src/main/java/app/ij/mlwithtensorflowlite/BloodMainActivity.java
@@ -0,0 +1,25 @@
+package app.ij.mlwithtensorflowlite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class BloodMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blood_main);
+
+    }
+}
---
+++ app/src/main/java/app/ij/mlwithtensorflowlite/SigatokaMainActivity.java
@@ -0,0 +1,14 @@
+package app.ij.mlwithtensorflowlite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SigatokaMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sigatoka_main);
+    }
+}
